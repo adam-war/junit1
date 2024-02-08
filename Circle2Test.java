2717d6b753a6cb8c3b34b6b742525acb2f2ef403
adam-war <adamwar@nmsu.edu>
2024-02-08 16:36:19 -0700

    Circle2 Simple Tests
    Adds tests inherent to Circle class to test on Circle2 class.

--- Circle2Test.java
+++ Circle2Test.java
@@ -29,6 +29,65 @@ public void teardown()
 
 // Tests begin here:
 
+//
+// Test a simple positive move
+//
+@Test
+public void simpleMove()
+{
+   Point p;
+   System.out.println("Running test simpleMove.");
+   p = circle1.moveBy(1,1);
+   assertTrue(p.x == 2 && p.y == 3);
+}
+
+// 
+// Test a simple negative move
+//
+@Test
+public void simpleMoveNeg()
+{
+   Point p;
+   System.out.println("Running test simpleMoveNeg.");
+   p = circle1.moveBy(-1,-1);
+   assertTrue(p.x == 0 && p.y == 1);
+}
+
+//
+// Test a more complex move
+//
+@Test
+public void largeMove()
+{
+   Point p;
+   System.out.println("Running test largeMove.");
+   p = circle1.moveBy(1,5);
+   assertTrue(p.x == 2 && p.y == 7);
+}
+
+//
+// Test a simple scale
+//
+@Test
+public void simpleScale()
+{
+   Circle2 p = circle1;
+   System.out.println("Running test simpleScale.");
+   p.scale(2.0);
+   assertTrue(p.radius == 6);
+}
+
+//
+// Test a negative scale factor
+//
+@Test
+public void negativeScale()
+{
+   Circle2 p = circle1;
+   System.out.println("Running test negativeScale.");
+   p.scale(0.5);
+   assertTrue(p.radius == 1.5);
+}
 
 
 
